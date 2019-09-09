8a8ce613da9fb46e8358ab2fc2e75526e11dff61
vbaimas <vpbaimas@gmail.com>
2019-09-09 10:31:28 +0300

    Add page without sidebar

---
+++ templates/page-no-sidebar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Template Name: Page - No Sidebar
+*
+* @package fashionclaire
+* @since fashionclaire 1.2.5
+*/
+get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>

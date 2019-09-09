acce40914be15b4b202446dafc3f4720cfa865f8
vbaimas <vpbaimas@gmail.com>
2019-09-09 10:17:19 +0300

    Add spaces in footer

--- footer.php
+++ footer.php
@@ -1,26 +1,26 @@
 </main>
-<footer id="colophon" class="site-footer"> 
-    <div class="container">
-        <div class="row">
-            <?php
-                /**
-                *
-                * @hooked fashionclaire_footer_start
-                */
-                do_action( 'fashionclaire_action_before_footer' );
-                /**
-                 * Hooked - fashionclaire_footer_top_section -10
-                * Hooked - fashionclaire_footer_section -20
-                */
-                do_action( 'fashionclaire_action_footer' );
-                /**
-                * Hooked - fashionclaire_footer_end. 
-                */
-                do_action( 'fashionclaire_action_after_footer' );
-            ?>
+    <footer id="colophon" class="site-footer"> 
+        <div class="container">
+            <div class="row">
+                <?php
+                    /**
+                    *
+                    * @hooked fashionclaire_footer_start
+                    */
+                    do_action( 'fashionclaire_action_before_footer' );
+                    /**
+                     * Hooked - fashionclaire_footer_top_section -10
+                    * Hooked - fashionclaire_footer_section -20
+                    */
+                    do_action( 'fashionclaire_action_footer' );
+                    /**
+                    * Hooked - fashionclaire_footer_end. 
+                    */
+                    do_action( 'fashionclaire_action_after_footer' );
+                ?>
+            </div>
         </div>
-     </div>
-</footer>
+    </footer>
 <?php wp_footer(); ?>
 </body>
 </html>

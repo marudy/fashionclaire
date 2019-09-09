224d9d44595f35605cf786c132a5aeb43dc11ac3
vbaimas <vpbaimas@gmail.com>
2019-09-09 19:23:41 +0300

    Remove some space

--- 404.php
+++ 404.php
@@ -3,7 +3,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
-       
                     <div class="text-center">
                          <h1 class="entry-title">
                             <?php esc_html_e('Page not Found', 'fashionclaire'); ?>

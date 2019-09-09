81ffae91c4e561f450fd18535208c7cceedca5dd
vbaimas <vpbaimas@gmail.com>
2019-09-09 14:10:25 +0300

    Remove span

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
     <div class="row">
         <div class="col-md-8">
             <?php if (have_posts()) : ?>
-                <h1 class="page-title"><?php printf(esc_attr('Search Results for: %s', 'fashionclaire'), '</span>' . get_search_query() . '</span>');?></h1>
+                <h1 class="page-title"><?php printf(esc_attr('Search Results for: %s', 'fashionclaire'), '<span>' . get_search_query() . '</span>');?></h1>
             <?php while (have_posts()) : the_post(); ?>
                 <?php get_template_part('template-parts/content-post', get_post_type() ); ?>
             <?php endwhile; ?>

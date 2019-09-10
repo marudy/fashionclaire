9f55863303ffaab1d53a5f0e16aba78986f00429
vbaimas <vpbaimas@gmail.com>
2019-09-10 16:38:05 +0300

    Change the descriptions of color options.

--- inc/customizer/theme-options.php
+++ inc/customizer/theme-options.php
@@ -22,7 +22,7 @@
     ));
     
     $wp_customize->add_control( 'show_site_title', array(
-        'label'       =>  esc_html__('Checked to show the title below the logo','fashionclaire'), 
+        'label'       =>  esc_html__('Check to show the title below the logo.','fashionclaire'), 
         'section'     =>  'title_tagline',
         'type'        =>  'checkbox',
         'priority'    =>  10,
@@ -35,7 +35,7 @@
     ));
     
     $wp_customize->add_control( 'show_site_tagline', array(
-        'label'      => esc_html__('Checked to show the tagline below the logo', 'fashionclaire'),
+        'label'      => esc_html__('Check to show the tagline below the logo.', 'fashionclaire'),
         'section'    => 'title_tagline',
         'type'       => 'checkbox',
         'priority'   => 10,
@@ -79,7 +79,7 @@
     ));
   
     $wp_customize->add_control('heading_font', array(
-        'label'         => __('Select the font family for Headings.', 'fashionclaire'),
+        'label'         => __('Select the font family for headings.', 'fashionclaire'),
         'description'   => __('Apply the font for the heading H1, H2, H3, H4, H5, H6. The default font family is PT Serif.', 'fashionclaire'),
         'section'       => 'fashionclaire_section_fonts',
         'type'          => 'select',
@@ -93,7 +93,7 @@
     ));
   
     $wp_customize->add_control('body_font', array(
-            'label'         => __('Select the font family for Body.', 'fashionclaire'),
+            'label'         => __('Select the font family for body.', 'fashionclaire'),
             'description'   => __('Apply the font for body. The default font family is PT Serif.', 'fashionclaire'),
             'section'       => 'fashionclaire_section_fonts',
             'type'          => 'select',
@@ -108,8 +108,8 @@
     ));
   
     $wp_customize->add_control('branding_font', array(
-            'label'         => __('Select the font family for Branding.', 'fashionclaire'),
-            'description'   => __('Apply the font for Site title and tagline. The default font family is PT Serif.', 'fashionclaire'),
+            'label'         => __('Select the font family for branding.', 'fashionclaire'),
+            'description'   => __('Apply the font for site title and tagline. The default font family is PT Serif.', 'fashionclaire'),
             'section'       => 'fashionclaire_section_fonts',
             'type'          => 'select',
             'choices'	 => fashionclaire_get_font_choices(),
@@ -131,7 +131,7 @@
   
     $wp_customize->add_control( 'show_top_header', array(
         'label'         =>  esc_html__('Show social media in top header','fashionclaire'),
-        'description'   =>  esc_html__('Checked to show the social media in the top of header', 'fashionclaire'),
+        'description'   =>  esc_html__('Check to show the social media in the top of header.', 'fashionclaire'),
         'section'       =>  'fashionclaire_top_header_section',
         'type'          => 'checkbox',
         'priority'      =>  10
@@ -152,7 +152,7 @@
   
    $wp_customize->add_control( 'show_single_sidebar', array(
         'label'       =>  esc_html__('Show sidebar in Single Post', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the right sidebar in single post', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the right sidebar in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_layout_section',
         'type'        =>  'checkbox',
     ));
@@ -165,7 +165,7 @@
       
    $wp_customize->add_control( 'show_page_sidebar', array(
         'label'       =>  esc_html__('Show sidebar in Page', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the right sidebar in page', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the right sidebar in page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_layout_section',
         'type'        =>  'checkbox',
     ));
@@ -185,7 +185,7 @@
     
     $wp_customize->add_control( 'show_archive_breadcrumb', array(
         'label'         =>  esc_html__('Show breadcrumb','fashionclaire'),
-        'description'   =>  esc_html__('Checked to show breadcrumb in archive/blog page.', 'fashionclaire'),
+        'description'   =>  esc_html__('Check to show breadcrumb in archive/blog page.', 'fashionclaire'),
         'section'       =>  'fashionclaire_blog_section',
         'type'          =>  'checkbox',  
     ));
@@ -198,7 +198,7 @@
   
     $wp_customize->add_control( 'show_content', array(
         'label'     =>  esc_html__('Show the content', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the content. The excerpt is the default option.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the content. The excerpt is the default option.', 'fashionclaire'),
         'section'   =>  'fashionclaire_blog_section',
         'type'      =>  'checkbox',
              )
@@ -242,7 +242,7 @@
   
     $wp_customize->add_control( 'continue_reading_text', array(
         'label'         =>  esc_html__('Button', 'fashionclaire'),
-        'description'   =>  esc_html__('Write the text for Continue Reading in archive/blog page.', 'fashionclaire'),
+        'description'   =>  esc_html__('Write the text for continue reading in archive/blog page.', 'fashionclaire'),
         'section'       =>  'fashionclaire_blog_section',
         'type'          =>  'text',
     ));
@@ -255,7 +255,7 @@
   
     $wp_customize->add_control( 'show_continue_reading_text', array(
         'label'         =>  esc_html__('Show the Continue Reading.', 'fashionclaire'),
-        'description'   =>  esc_html__('Checked to show the Continue Reading in archive/blog page.', 'fashionclaire'),
+        'description'   =>  esc_html__('Check to show the continue reading in archive/blog page.', 'fashionclaire'),
         'section'       =>  'fashionclaire_blog_section',
         'type'          =>  'checkbox',
     ));
@@ -268,7 +268,7 @@
   
     $wp_customize->add_control( 'show_archive_category', array(
         'label'       =>  esc_html__('Show the categories.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show categories in archive/blog page.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the categories in archive/blog page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_blog_section',
         'type'        =>  'checkbox',
     ));
@@ -281,7 +281,7 @@
   
     $wp_customize->add_control( 'show_archive_date', array(
         'label'       =>  esc_html__('Show the published date.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the published date in archive/blog page.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the published date in archive/blog page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_blog_section',
         'type'        =>  'checkbox',
     ));
@@ -294,7 +294,7 @@
   
     $wp_customize->add_control( 'show_archive_comments', array(
         'label'       =>  esc_html__('Show the count of comments.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the count of comments in archive/blog page.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the count of comments in archive/blog page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_blog_section',
         'type'        =>  'checkbox',
     ));
@@ -307,7 +307,7 @@
   
     $wp_customize->add_control( 'show_archive_featured_image', array(
         'label'       =>  esc_html__('Show the featured image.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the featured image in archive/blog page.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the featured image in archive/blog page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_blog_section',
         'type'        =>  'checkbox',
     ));
@@ -328,7 +328,7 @@
     
     $wp_customize->add_control( 'show_single_breadcrumb', array(
         'label'         =>  esc_html__('Show breadcrumb.', 'fashionclaire'),
-        'description'   =>  esc_html__('Checked to show breadcrumb in single post.', 'fashionclaire'),
+        'description'   =>  esc_html__('Check to show the breadcrumb in single post.', 'fashionclaire'),
         'section'       =>  'fashionclaire_single_post_section',
         'type'          =>  'checkbox',  
     ));
@@ -341,7 +341,7 @@
   
     $wp_customize->add_control( 'show_single_category', array(
         'label'       =>  esc_html__('Show the categories.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show categories in single post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the categories in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_single_post_section',
         'type'        =>  'checkbox',
     ));
@@ -354,7 +354,7 @@
   
     $wp_customize->add_control( 'show_single_date', array(
         'label'       =>  esc_html__('Show the published date.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the published date in single post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the published date in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_single_post_section',
         'type'        =>  'checkbox',
     ));
@@ -367,7 +367,7 @@
   
     $wp_customize->add_control( 'show_single_comments', array(
             'label'       =>  esc_html__('Show the count of comments.', 'fashionclaire'),
-            'description' =>  esc_html__('Checked to show the count of comments in single post.', 'fashionclaire'),
+            'description' =>  esc_html__('Check to show the count of comments in single post.', 'fashionclaire'),
             'section'     =>  'fashionclaire_single_post_section',
             'type'        =>  'checkbox',
     ));
@@ -380,7 +380,7 @@
   
     $wp_customize->add_control( 'show_single_tags', array(
         'label'       =>  esc_html__('Show the tags.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show tags in single post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show tags in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_single_post_section',
         'type'        =>  'checkbox',
     ));
@@ -393,7 +393,7 @@
   
     $wp_customize->add_control( 'show_single_author', array(
         'label'       =>  esc_html__('Show the author biography.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the author biography in single post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the author biography in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_single_post_section',
         'type'        =>  'checkbox',
     ));
@@ -406,7 +406,7 @@
   
     $wp_customize->add_control( 'show_single_pagination', array(
         'label'       =>  esc_html__('Show the pagination.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the pagination in single post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the pagination in single post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_single_post_section',
         'type'        =>  'checkbox',
     ));
@@ -426,7 +426,7 @@
     
     $wp_customize->add_control( 'show_page_breadcrumb', array(
         'label'         =>  esc_html__('Show breadcrumb', 'fashionclaire'),
-        'description'   =>  esc_html__('Checked to show breadcrumb in page.', 'fashionclaire'),
+        'description'   =>  esc_html__('Check to show breadcrumb in page.', 'fashionclaire'),
         'section'       =>  'fashionclaire_page_section',
         'type'          =>  'checkbox',  
     ));
@@ -439,7 +439,7 @@
   
     $wp_customize->add_control( 'show_page_featured_image', array(
         'label'       =>  esc_html__('Show featured image.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the featured image in page.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the featured image in page.', 'fashionclaire'),
         'section'     =>  'fashionclaire_page_section',
         'type'        =>  'checkbox',
     ));
@@ -460,7 +460,7 @@
     
     $wp_customize->add_control( 'change_related_title', array(
         'label'       =>  esc_html__('Related posts section title.', 'fashionclaire'),
-        'description' =>  esc_html__('Change the related posts section title.', 'fashionclaire'),
+        'description' =>  esc_html__('Change the title of related posts section.', 'fashionclaire'),
         'section'     =>  'fashionclaire_related_section',
         'type'        =>  'text',
     ));
@@ -473,7 +473,7 @@
     
     $wp_customize->add_control( 'show_related_date', array(
         'label'       =>  esc_html__('Show the date.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the date of related post.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the date of related post.', 'fashionclaire'),
         'section'     =>  'fashionclaire_related_section',
         'type'        =>  'checkbox',
     ));
@@ -486,7 +486,7 @@
     
     $wp_customize->add_control( 'show_related_content', array(
         'label'       =>  esc_html__('Show the related posts section.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the section of related posts.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the section of related posts.', 'fashionclaire'),
         'section'     =>  'fashionclaire_related_section',
         'type'        =>  'checkbox',
     ));
@@ -506,7 +506,7 @@
     
     $wp_customize->add_control( 'show_entry_date_widget', array(
         'label'       =>  esc_html__('Show the date of posts widgets.', 'fashionclaire'),
-        'description' =>  esc_html__('Checked to show the date of posts in widget section.', 'fashionclaire'),
+        'description' =>  esc_html__('Check to show the date of posts in widget section.', 'fashionclaire'),
         'section'     =>  'fashionclaire_extra_options',
         'type'        =>  'checkbox',
     ));
@@ -534,7 +534,7 @@
     // Add Responsive section
     $wp_customize->add_section('fashionclaire_responsive_section',array(
         'title'         =>  esc_html__('Responsive Options', 'fashionclaire'),
-        'description'   =>  esc_html__('These options apply only for Mobile devices.', 'fashionclaire'),
+        'description'   =>  esc_html__('These options apply only for mobile devices.', 'fashionclaire'),
         'panel'         =>  'fashionclaire_theme_panel',
     ));
   
@@ -545,7 +545,7 @@
     ));
     
     $wp_customize->add_control( 'hide_mobile_top_header', array(
-        'label'     =>  esc_html__('Checked to hide the top header section.', 'fashionclaire'),
+        'label'     =>  esc_html__('Check to hide the top header section.', 'fashionclaire'),
         'section'   =>  'fashionclaire_responsive_section',
         'type'      =>  'checkbox',
     ));
@@ -557,7 +557,7 @@
     ));
     
     $wp_customize->add_control( 'hide_mobile_related_content', array(
-        'label'       =>    esc_html__('Checked to hide the related posts section.', 'fashionclaire'),
+        'label'       =>    esc_html__('Check to hide the related posts section.', 'fashionclaire'),
         'section'     =>    'fashionclaire_responsive_section',
         'type'        =>    'checkbox',
     ));
@@ -569,7 +569,7 @@
     ));
     
     $wp_customize->add_control( 'hide_mobile_sidebar', array(
-        'label'       =>    esc_html__('Checked to hide the sidebar.', 'fashionclaire'),
+        'label'       =>    esc_html__('Check to hide the sidebar.', 'fashionclaire'),
         'section'     =>    'fashionclaire_responsive_section',
         'type'        =>    'checkbox',
     ));

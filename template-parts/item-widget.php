5ee1bdd4496c2320dbea70e8094b477c8215954f
vbaimas <vpbaimas@gmail.com>
2019-09-10 18:48:20 +0300

    Remove the class from figure

--- template-parts/item-widget.php
+++ template-parts/item-widget.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-sm-5 col-xs-12">
             <?php if (has_post_thumbnail() ):?>
-                <figure class="entry-thumb-widget">
+                <figure>
                     <a href="<?php the_permalink(); ?>"tabindex="-1" aria-hidden="true">
                         <?php the_post_thumbnail( 'fashionclaire-xsmall', [ 'alt' => get_the_title() ] ); ?>
                     </a>
@@ -12,9 +12,7 @@
         <div class="col-sm-7 col-xs-12">
             <div class="entry-widget-content">
                 <?php if((fashionclaire_get_option('show_entry_date_widget'))==1) :  ?>
-                    <div class="entry-time-widget">
                          <?php fashionclaire_posted_on();?>
-                    </div>
                 <?php endif; ?>
                 <?php the_title( '<h1 class="entry-title-widget"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' ); ?>
             </div>

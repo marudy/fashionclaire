8633f52f6bd0d8631ff8e389b1909003f051b0c1
vbaimas <vpbaimas@gmail.com>
2019-09-10 10:29:46 +0300

    Remove span from comments

--- comments.php
+++ comments.php
@@ -33,14 +33,14 @@ if ( post_password_required() ) {
                     printf(
                         /* translators: 1: title. */
                         esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'fashionclaire' ),
-                        '<span>' .  esc_html (get_the_title()) . '</span>'
+                        '' .  esc_html (get_the_title()) . ''
                     );
                 } else {
                     printf( // WPCS: XSS OK.
                         /* translators: 1: comment count number, 2: title. */
                         esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'fashionclaire' ) ),
                         esc_attr (number_format_i18n( $comment_count )),
-                        '<span>' . esc_html (get_the_title()) . '</span>'
+                        '' . esc_html (get_the_title()) . ''
                     );
                 }
                 ?>

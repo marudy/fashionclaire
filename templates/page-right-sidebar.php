270f64b768af823bf17a8d68bb6f0eed9a819d08
vbaimas <vpbaimas@gmail.com>
2019-09-11 17:08:22 +0300

    remove columns

--- templates/page-right-sidebar.php
+++ templates/page-right-sidebar.php
@@ -8,7 +8,7 @@
 get_header(); ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-sm-7">
+			<div class="col-md-8">
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 			</div>
 			<?php get_sidebar(); ?>

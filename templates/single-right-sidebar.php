fc59e0d9a2a258b1895d65d2f462e0811602adf4
vbaimas <vpbaimas@gmail.com>
2019-09-11 17:12:08 +0300

    Remove columns, update the version

--- templates/single-right-sidebar.php
+++ templates/single-right-sidebar.php
@@ -4,12 +4,12 @@
 * Template Post Type: post
 *
 * @package fashionclaire
-* @since fashionclaire 1.2.0
+* @since fashionclaire 1.2.5
 */
 get_header(); ?>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-8 col-sm-7">
+			<div class="col-md-8">
 				<?php get_template_part( 'template-parts/content' , 'single', get_post_format() ); ?>
 			</div>
 			<?php get_sidebar(); ?>

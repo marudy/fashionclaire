5e51c3aca53801bdd5401648009e695aca0977db
vbaimas <vpbaimas@gmail.com>
2019-09-11 16:30:56 +0300

    Update the alt attr

--- template-parts/item-related.php
+++ template-parts/item-related.php
@@ -2,7 +2,7 @@
     <?php if (has_post_thumbnail() ):?>
         <figure class="entry-item-thumb">
             <a href="<?php the_permalink(); ?>"tabindex="-1" aria-hidden="true">
-                <?php the_post_thumbnail( 'fashionclaire-xsmall', [ 'alt' => get_the_title() ] ); ?>
+                <?php the_post_thumbnail( 'fashionclaire-xsmall', [ 'alt' => esc_html ( get_the_title() ) ] ); ?>
             </a>
         </figure>
     <?php endif;?>

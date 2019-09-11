058febafe3a57088c36d44ec5a15983c41025d07
vbaimas <vpbaimas@gmail.com>
2019-09-11 17:17:06 +0300

    Remove html

--- archive.php
+++ archive.php
@@ -8,7 +8,7 @@
                     </div>
                     <?php endif;?>
                     <?php if(is_archive() ) : ?>
-                        <h2 class="section-title"><?php the_archive_title(); ?> </h2>
+                        <h2><?php the_archive_title(); ?> </h2>
                         <?php endif ;?>
                         <?php if (have_posts() ) : while ( have_posts() ) : the_post();
                             get_template_part('template-parts/content' , 'post', get_post_format() );

edff3b5853c37cf4241bd93f71ac437cb546b722
vbaimas <vpbaimas@gmail.com>
2019-09-11 17:31:15 +0300

    Rename the file and slug

---
+++ templates/right-sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Template Name: Right Sidebar
+*
+* @package fashionclaire
+* @since fashionclaire 1.2.5
+*/
+get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			</div>
+			<?php get_sidebar(); ?>
+		</div>
+	</div>
+<?php get_footer(); ?>
